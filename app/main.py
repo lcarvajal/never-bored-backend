e6607165d768655e5b25325cc2d5290c0aa498f5
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-14 01:25:25 +0200

    Configure Firebase authentication

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from router import router
+from fastapi import FastAPI
+import firebase_admin
+from pathlib import  Path
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Union
+from config import get_settings
+
+app = FastAPI()
+app.include_router(router)
+settings = get_settings()
+origins = [settings.frontend_url]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+firebase_admin.initialize_app()
+
+# Debug, check app is correctly
+print("Current App Name:", firebase_admin.get_app().project_id)
