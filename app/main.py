3c112d03b32385ea995338591f5ab5a6e33e94ca
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-14 01:55:12 +0200

    Move code from main back to router

--- app/main.py
+++ app/main.py
@@ -1,26 +1,10 @@
-from typing import Annotated
-from app.config import get_firebase_user_from_token
-from fastapi import FastAPI,APIRouter, Depends
+from app.router import router
+from fastapi import FastAPI
 import firebase_admin
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import get_settings
 
 app = FastAPI()
-
-router = APIRouter()
-
-
-@router.get("/")
-def hello():
-    """Hello world route to make sure the app is working correctly"""
-    return {"msg": "Hello World!"}
-
-
-@router.get("/userid")
-async def get_userid(user: Annotated[dict, Depends(get_firebase_user_from_token)]):
-    """gets the firebase connected user"""
-    return {"id": user["uid"]}
-
 app.include_router(router)
 settings = get_settings()
 origins = [settings.frontend_url]
@@ -34,3 +18,6 @@ app.add_middleware(
 )
 
 firebase_admin.initialize_app()
+
+# Debug, check app is correctly
+print("Current App Name:", firebase_admin.get_app().project_id)
