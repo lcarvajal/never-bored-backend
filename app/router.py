ae47c216a6e56805c588672fb1d71f389192fd11
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-14 14:58:07 +0200

    Add route to post profile

--- app/router.py
+++ app/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from typing import Annotated
 from app.config import get_firebase_user_from_token, upload_blob
 import json
+from pydantic import BaseModel
 
 router = APIRouter()
 
@@ -10,21 +11,36 @@ def hello():
     """Hello world route to make sure the app is working correctly"""
     return {"msg": "Hello World!"}
 
-@router.post("/profile")
-def post_profile(user: Annotated[dict, Depends(get_firebase_user_from_token)]):
-    """Uploads profile to azure blob storage"""
-    uid = user["uid"]
-    email = user["email"]
+class Profile(BaseModel):
+    name: str
+    email: str
+    goal: str
+    reason: str
+    deadline: str
+    lastLearnedDescription: str
 
-    file_name = f'{uid}.json'
+@router.post("/profiles")
+def post_profile(profile: Profile):
+    """Uploads profile to azure blob storage"""
+    file_name = f'profile-{profile.email}.json'
     profile = {
-        "email": email,
-        "uid": uid
+        "name": profile.name,
+        "email": profile.email,
+        "goal": profile.goal,
+        "reason": profile.reason,
+        "deadline": profile.deadline,
+        "lastLearnedDescription": profile.lastLearnedDescription
     }
     file_content = json.dumps(profile)
 
     upload_blob(file_name, file_content)
 
+# @router.get("/roadmap")
+# def post_profile(user: Annotated[dict, Depends(get_firebase_user_from_token)]):
+#     """Generates a learning path based on the user profile"""
+#     file_name = "username.json"
+#     file_content = '{"email": "p5q5y@example.com", "id": "username"}'
+#     upload_blob(file_name, file_content)
 
 @router.get("/userid")
 async def get_userid(user: Annotated[dict, Depends(get_firebase_user_from_token)]):
