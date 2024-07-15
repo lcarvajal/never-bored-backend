cc224869384837bf8b7678cc6342d6750beb974e
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-15 15:53:20 +0200

    Attempt to reduce size of app

--- app/authentication.py
+++ app/authentication.py
@@ -1,7 +1,10 @@
 from typing import Annotated, Optional
 from fastapi import Depends, HTTPException, status
-from fastapi.security import HTTPAuthorizationCredentials
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from firebase_admin import auth
+
+# Use a simple bearer scheme as auth is handled by firebase and not fastapi
+bearer_scheme = HTTPBearer(auto_error=False)
 
 def get_firebase_user_from_token(
     token: Annotated[Optional[HTTPAuthorizationCredentials], Depends(bearer_scheme)],
@@ -22,7 +25,6 @@ def get_firebase_user_from_token(
         user = auth.verify_id_token(token.credentials)
         return user
     except Exception as e:
-        logger.error(f"Error verifying token: {str(e)}")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Not logged in or Invalid credentials",
--- app/config.py
+++ app/config.py
@@ -1,21 +1,13 @@
-import os, logging
+import os
 from functools import lru_cache
-from fastapi.security import HTTPBearer
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 load_dotenv()
-
-# Use a simple bearer scheme as auth is handled by firebase and not fastapi
-bearer_scheme = HTTPBearer(auto_error=False)
 
 class Settings(BaseSettings):
     """Main settings"""
-    app_name: str = "demofirebase"
+    app_name: str = "never-bored-learning"
     env: str = os.getenv("ENV", "development")
     frontend_url: str = os.getenv("FRONTEND_URL", "NA")
 
