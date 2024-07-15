50922c725e095010cc8c4132b5abbc3ea450f0ed
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-15 17:33:09 +0200

    Add debugging errors

--- app/storage.py
+++ app/storage.py
@@ -12,6 +12,11 @@ def upload_blob(file_name, file_content):
         blob_client = blob_service_client.get_blob_client(container="user-profile", blob=file_name)
         blob_client.upload_blob(file_content, overwrite=True)
 
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_418_IM_A_TEAPOT,
+            detail="Azure connection not made.",
+        )
     except:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -28,6 +33,11 @@ async def download_blob(file_name, container):
         blob_client = blob_service_client.get_blob_client(container, blob=file_name)
         blob_data = blob_client.download_blob().readall()
         return blob_data
+    except ValueError as e:
+        raise HTTPException(
+            status_code=status.HTTP_418_IM_A_TEAPOT,
+            detail="Azure connection not made.",
+        )
     except:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
