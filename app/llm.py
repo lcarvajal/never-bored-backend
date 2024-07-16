e1f1896a0081391fc9d11401e7513a3928d7b67d
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-16 14:35:50 +0200

    Add post categories route

--- app/llm.py
+++ app/llm.py
@@ -18,7 +18,6 @@ def get_roadmap(learning_goal: str):
   
   parser = JsonOutputParser(pydantic_object=StudyPlan)
 
-  # A chat template converts raw user input into better input for an llm.
   prompt = PromptTemplate(
       template="Break down the learning goal into smaller learning goals using Blooms taxonomy verbs: \n{format_instructions}\n{learning_goal}\n",
       input_variables=["learning_goal"],
@@ -27,3 +26,24 @@ def get_roadmap(learning_goal: str):
 
   chain = prompt | model | parser
   return chain.invoke({"learning_goal": learning_goal})
+
+def get_categories(learning_goal: str, roadmap_item_name: str, roadmap_item_description: str):
+  model = ChatOpenAI()
+
+  class Categories(BaseModel):
+      categories: List[str]
+  
+  parser = JsonOutputParser(pydantic_object=Categories)
+
+  prompt = PromptTemplate(
+      template="A user has the learning goal `{learning_goal}` and roadmap item `{roadmap_item_name}` with description `{roadmap_item_description}`. Break the roadmap description into a list of categories for grouping learning resources. Keep the category names short since they will be used on buttons:\n{format_instructions}\n",
+      input_variables=["learning_goal", "roadmap_item_name", "roadmap_item_description"],
+      partial_variables={"format_instructions": parser.get_format_instructions()},
+  )
+
+  chain = prompt | model | parser
+  return chain.invoke({
+     "learning_goal": learning_goal, 
+     "roadmap_item_name": roadmap_item_name, 
+     "roadmap_item_description": roadmap_item_description
+     })
