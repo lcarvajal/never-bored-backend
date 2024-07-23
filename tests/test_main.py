9dc4fca11ad2ae3a733158a4490299c9d8e31736
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-23 17:17:12 +0200

    Add dependency override for authentication

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,20 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from app.utils.authentication import get_firebase_user_from_token
 
 
 client = TestClient(app)
 
+async def override_authentication_dependency(q: str | None = None):
+    return {
+        "uid": "1234",
+        "provider_id": "firebase",
+        "display_name": "Ricky Bobby", 
+        "email": "test@neverbored.com", 
+        "photo_url": None
+    }
+
+app.dependency_overrides[get_firebase_user_from_token] = override_authentication_dependency
 
 def test_read_main():
     response = client.get("/")
