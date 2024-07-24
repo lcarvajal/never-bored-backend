67a13ec512f88a02564778389dccc43fb0c525a1
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-24 11:16:20 +0200

    Configure admin for modules, submodules, and resources

--- app/main.py
+++ app/main.py
@@ -54,5 +54,17 @@ class UserAdmin(ModelView, model=user.User):
 class RoadmapAdmin(ModelView, model=roadmap.Roadmap):
     column_list = [roadmap.Roadmap.id, roadmap.Roadmap.title]
 
+class ModuleAdmin(ModelView, model=roadmap.Module):
+    column_list = [roadmap.Module.id, roadmap.Module.title]
+
+class SubmoduleAdmin(ModelView, model=roadmap.Submodule):
+    column_list = [roadmap.Submodule.id, roadmap.Submodule.title]
+
+class ResourceAdmin(ModelView, model=roadmap.Resource):
+    column_list = [roadmap.Resource.id, roadmap.Resource.title, roadmap.Resource.type, roadmap.Resource.url]
+
 admin.add_view(UserAdmin)
 admin.add_view(RoadmapAdmin)
+admin.add_view(ModuleAdmin)
+admin.add_view(SubmoduleAdmin)
+admin.add_view(ResourceAdmin)
