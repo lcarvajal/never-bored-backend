e23d0ab9e5aed10d48cd5e9942c88031329206b0
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-26 23:54:09 +0200

    Remove debugging print statements

--- app/utils/ragsearch.py
+++ app/utils/ragsearch.py
@@ -2,9 +2,6 @@ from tavily import TavilyClient
 import os
 
 tavily_key = os.getenv("TAVILY_API_KEY")
-print("WHAT IS GOING ON HERE")
-print(tavily_key)
-print(os)
 tavily_client = TavilyClient(api_key=tavily_key)
 
 def get_search_resources(learning_topic: str):
