e28202a8ea7b42f386f02aec5cee674778ae3fe9
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-26 23:46:15 +0200

    Remove problem line from startup.sh

--- startup.sh
+++ startup.sh
@@ -3,8 +3,5 @@
 # Export all environment variables safely
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
-# Source the environment variables
-. /etc/profile.d/env.sh
-
 # Start the FastAPI application
 uvicorn app.main:app --host 0.0.0.0 --port 8000
