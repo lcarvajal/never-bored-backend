b5ea612d1eff6ea4d6d7d9cacc34db02ae0ed2cc
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-31 18:20:30 +0200

    Configure logging for startup script

--- startup.sh
+++ startup.sh
@@ -12,7 +12,7 @@ if [ "$ENV" = "dev" ]; then
     echo "Starting FastAPI in development mode with SSL..."
     SSL_KEYFILE="./certs/key.pem"
     SSL_CERTFILE="./certs/cert.pem"
-    uvicorn app.main:app --host 0.0.0.0 --port 443 --ssl-keyfile=$SSL_KEYFILE --ssl-certfile=$SSL_CERTFILE
+    uvicorn app.main:app --host 0.0.0.0 --port 443 --ssl-keyfile=$SSL_KEYFILE --ssl-certfile=$SSL_CERTFILE --log-config ./logging.conf
 else
     # Export all environment variables safely for Azure
     eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
@@ -24,5 +24,5 @@ else
     apply_migrations
     
     echo "Starting FastAPI in production mode..."
-    uvicorn app.main:app --host 0.0.0.0 --port 8000
+    uvicorn app.main:app --host 0.0.0.0 --port 8000 --log-config ./logging.conf
 fi
