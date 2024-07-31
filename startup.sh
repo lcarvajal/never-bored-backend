685f273a785ed44e877adb797756b4a9ee247eeb
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-31 14:27:45 +0200

    Run apply migrations on prod app start

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+apply_migrations() {
+    echo "Applying database migrations..."
+    if ! alembic upgrade head; then
+        echo "Error applying migrations"
+        exit 1
+    fi
+}
+
 if [ "$ENV" = "dev" ]; then
     echo "Starting FastAPI in development mode with SSL..."
     SSL_KEYFILE="./certs/key.pem"
@@ -11,6 +19,9 @@ else
 
     # Set the default environment to production if not specified
     ENV=${ENV:-production}
+
+    # Apply database migrations
+    apply_migrations
     
     echo "Starting FastAPI in production mode..."
     uvicorn app.main:app --host 0.0.0.0 --port 8000
